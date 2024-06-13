3b17fef171e79266600ece28477006d096aec631
peach <mbezema@protonmail.com>
2024-06-13 11:14:22 +0200

    migrate install scripts

---
+++ ansible_install/setup_terminal.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install zsh git curl tmux
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone git@github.com:mennobo/scripts.git
+cp scripts/config .tmux.config ~/
+cp scripts/config .zshrc ~/
+
+
